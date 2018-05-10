ead193c762710f555cbcd0645a84a8e736a87ce5
aaruff <anwarruff@gmail.com>
2018-05-10 15:52:18 -0400

    Added generation of problem equations

--- experiment/models.py
+++ experiment/models.py
@@ -1,3 +1,5 @@
+import random
+
 from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
@@ -15,10 +17,35 @@ class Constants(BaseConstants):
     name_in_url = 'experiment'
     players_per_group = None
     num_rounds = 1
+    num_expressions_per_cell = 10
+    num_problems = 4
 
 
 class Subsession(BaseSubsession):
-    pass
+    def easy_int(self):
+        return random.randint(1, 10)
+
+    def hard_int(self):
+        return random.randint(10, 1000)
+
+    def creating_session(self):
+        problem_types = [True, True, False, False]
+        for player in self.get_players():
+            problems = []
+            for pid in range(Constants.num_problems):
+                expressions = []
+                for eid in range(Constants.num_expressions_per_cell):
+                    op = random.choice([' + ', ' - ', ' * '])
+                    if problem_types[pid]:
+                        operand1 = self.hard_int()
+                        operand2 = self.hard_int()
+                    else:
+                        operand1 = self.easy_int()
+                        operand2 = self.easy_int()
+
+                    expressions.append("{} {} {}".format(operand1, op, operand2))
+                problems.append(expressions)
+            player.participant.vars['problems'] = problems
 
 
 class Group(BaseGroup):
@@ -26,4 +53,4 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    problem = models.IntegerField(min=1, max=4)
+    problem = models.IntegerField(min=0, max=4)
